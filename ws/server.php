f4befa8f43096ce384b9b4593d862dc39197b7f2
Jorge Castro <jorge.castro.c@gmail.com>
2020-05-03 20:07:18 -0400

    adding pagination

--- ws/server.php
+++ ws/server.php
@@ -8,8 +8,8 @@ include '../vendor/autoload.php';
 include '../repo/CountryRepo.php';
 include '../repo/PlayersRepo.php';
 
-$pdoOne=new PdoOne('mysql','127.0.0.1','root','abc.123','example_editable_grid');
-$pdoOne->logLevel=3;
+$pdoOne = new PdoOne('mysql', '127.0.0.1', 'root', 'abc.123', 'example_editable_grid');
+$pdoOne->logLevel = 3;
 try {
     $pdoOne->connect();
 } catch (Exception $e) {
@@ -17,72 +17,74 @@ try {
     die(1);
 }
 
-$action=@$_GET['action'];
+$action = @$_GET['action'];
 
 //$countries=CountryRepo::toList();
 //var_dump($countries);
 
 switch ($action) {
     case 'add':
-        $record=PlayersRepo::factoryNull(); // we create an empty array
-    
+        $record = PlayersRepo::factoryNull(); // we create an empty array
+
         try {
             PlayersRepo::insert($record);
-            $result=['result'=>true];
+            $result = ['result' => true];
         } catch (Exception $e) {
-            $result=['result'=>false,'message'=>$e->getMessage()];
+            $result = ['result' => false, 'message' => $e->getMessage()];
         }
         echo json_encode($result);
         break;
     case 'save':
-        $record=$_POST['record'];
+        $record = $_POST['record'];
         unset($record['CountryName']); // we delete the field countryname
-        $record['IsActive']=($record['IsActive'])?1:0; // we convert isactive(boolean) to 1 or 0.
+        $record['IsActive'] = ($record['IsActive']) ? 1 : 0; // we convert isactive(boolean) to 1 or 0.
         try {
             PlayersRepo::update($record);
-            $result=['result'=>true];
+            $result = ['result' => true];
         } catch (Exception $e) {
-            $result=['result'=>false,'message'=>$e->getMessage()];
+            $result = ['result' => false, 'message' => $e->getMessage()];
         }
-        
+
         echo json_encode($result);
         break;
     case 'delete':
-        $id=$_POST['id'];
+        $id = $_POST['id'];
         try {
             PlayersRepo::deleteById($id);
-            $result=['result'=>true];
+            $result = ['result' => true];
         } catch (Exception $e) {
-            $result=['result'=>false,'message'=>$e->getMessage()];
+            $result = ['result' => false, 'message' => $e->getMessage()];
         }
-        
+
         echo json_encode($result);
         break;
     case 'get':
-        $r=[];
-        $page=isset($_GET['page']) ? $_GET['page'] : 1;
-        $limit=isset($_GET['limit']) ? $_GET['limit'] : 5;
-        $rowInit=($limit*($page-1));
+        $r = [];
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+        $limit = isset($_GET['limit']) ? $_GET['limit'] : 5;
+        $rowInit = ($limit * ($page - 1));
         try {
-            $result['records'] = $pdoOne->select('ID,Players.Name,Country.Name as CountryName,IsActive')
-                                    ->limit("$rowInit,$limit")
-                                   ->from('Players')
-                                   ->left('Country on Players.IdCounty=Country.IdCounty')->toList();
-            $result['total']=count($result['records']);
+            $result['records'] =
+                $pdoOne->select('ID,Players.Name,Country.Name as CountryName,IsActive')
+                       ->limit("$rowInit,$limit")
+                       ->from('Players')
+                       ->left('Country on Players.IdCounty=Country.IdCounty')
+                       ->toList();
+            $result['total'] = count($result['records']);
         } catch (Exception $e) {
-            $result=['result'=>false,'message'=>$e->getMessage()];
+            $result = ['result' => false, 'message' => $e->getMessage()];
         }
-        
+
         echo json_encode($result);
         break;
     case 'countries':
         try {
             $result = CountryRepo::toList();
         } catch (Exception $e) {
-            $result=['result'=>false,'message'=>$e->getMessage()];
+            $result = ['result' => false, 'message' => $e->getMessage()];
         }
         echo json_encode($result);
         break;
     default:
-        echo json_encode(['result'=>false,'message'=>'???? '.$action]);
+        echo json_encode(['result' => false, 'message' => '???? ' . $action]);
 }
